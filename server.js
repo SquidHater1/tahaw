51ef175db08de768321ff1129e173eb229fbfe2e
Ryan Smith <smithry9@oregonstate.edu>
2020-12-08 14:56:32 -0800

    added database stroage to server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+var fs = require('fs');
 var path = require('path');
 var express = require('express');
 var exphbs = require('express-handlebars');
 
-var scoreData = require('./scoreData.json')
+
 
 var app = express();
 var port = process.env.PORT || 3000;
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/:game', function(req, res, next){
+	var scoreData = require('./scoreData.json')
 	var game = req.params.game.toLowerCase();
 	var gameData;
 	if(game == "ta"){
@@ -36,7 +38,16 @@ app.post('/:game/addScore', function(req, res, next){
 	if(game == "ta" || game == "haw"){
 		if (req.body && req.body.name && req.body.score){
 			var scoreValues = req.body;
-			res.status(200);
+			//res.status(200).send("Successfully got score for "+ game + ": " + scoreValues.name + ", " + scoreValues.score);
+
+			var response = storeScore(scoreValues, game);
+			console.log("==Response: ", response);
+			if (response == 200){
+				res.status(200).send("Score stored successfully");
+			}else{
+				res.status(500).send("Error storing score");
+			}
+
 		}else{
 			res.status(400).send("Request must have 'name' and 'score' values");
 		}
@@ -44,6 +55,43 @@ app.post('/:game/addScore', function(req, res, next){
 		res.status(404).send("Request must be one of the games");
 	}
 });
+
+
+function storeScore(scoreValues, game){
+	var scoreData = require('./scoreData.json')
+	var gameData;
+	if(game == "ta"){
+		gameData = scoreData.ta;
+	}else{
+		gameData = scoreData.haw;
+	}
+
+	gameData.push({
+		name: scoreValues.name,
+		score: scoreValues.score
+	});
+
+	if(game == "ta"){
+		scoreData.ta = gameData;
+	}else{
+		scoreData.haw = gameData;
+	}
+
+	console.log("Score data: ", scoreData);
+
+
+	fs.writeFile(
+		__dirname + '/scoreData.json',
+		JSON.stringify(scoreData, null, 2),
+		function (err, data) {
+			if(err){
+				console.log(" --err:", err);
+				return 500;
+			}
+		}
+	);
+	return 200;
+}
 
 
 app.get('*', function(req, res){
