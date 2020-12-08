8af5d5b881bcf57d92ea952eee79a5b695be79c0
Ryan Smith <smithry9@oregonstate.edu>
2020-12-07 22:48:09 -0800

    Server accepts urls

--- server.js
+++ server.js
@@ -8,10 +8,23 @@ var port = process.env.PORT || 3000;
 app.engine('handlebars', exphbs({ default: null}));
 app.set('view engine', 'handlebars');
 
+
+app.use(express.json());
 app.use(express.static('public'));
 
+app.get('/:game', function(req, res, next){
+	var game = req.params.game.toLowerCase();
+	if(game == "ta" || game == "haw"){
+		console.log("== Game Requested: ", game);
+		res.status(200).sendFile(path.join(__dirname+'/'+game+'.html'));
+	}else{
+		next();
+	}
+});
 
-
+app.get('*', function(req, res){
+	res.status(404).sendFile(path.join(__dirname + '/404.html'));
+});
 
 app.listen(port, function() {
 	console.log("== Server is listening on port", port);
