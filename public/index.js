6b6ac433d8f522d20ee99ce349121794be766de8
Ryan Smith <smithry9@oregonstate.edu>
2020-12-08 16:29:12 -0800

    added button support for options modal

---
+++ public/index.js
@@ -0,0 +1,12 @@
+function createEventListeners(){
+	var optionsButton = document.getElementById('options_button');
+	optionsButton.addEventListener('click', handleOptionsButtonClicked);
+}
+
+function handleOptionsButtonClicked(event){
+	var optionsModal = document.getElementById('options_modal');
+	var backdrop = document.getElementById('modal-backdrop');
+	backdrop.classList.remove("hidden");
+	optionsModal.classList.remove("hidden");
+	console.log("== Options Modal Opened!");
+}
